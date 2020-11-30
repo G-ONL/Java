3b7049bcf32fc03dc4aed5ae374bbac14d21250d
kimjaeyun <je941004@naver.com>
2020-11-30 23:20:35 +0900

    LinkedList 구현 해보기 1

---
+++ java/com/example/java/LinkedList.java
@@ -0,0 +1,23 @@
+package com.example.java;
+
+public class LinkedList {
+
+
+  private ListNode add(ListNode head, ListNode nodeToAdd, int position){
+
+    return null;
+  }
+  private ListNode remove(ListNode head, int positionToRemove){
+    return null;
+  }
+  private boolean contains(ListNode head, ListNode nodeToCheck){
+    return false;
+  }
+}
+
+class ListNode{
+
+  public ListNode next() {
+    return null;
+  }
+}
