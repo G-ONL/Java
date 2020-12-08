bb4cd467ad0a3b81534efe5fbb06c20296f038ec
kimjaeyun <je941004@naver.com>
2020-12-08 23:51:16 +0900

    만든 node로 구현하는 stack

---
+++ src/main/java/com/example/java/StackByNode.java
@@ -0,0 +1,32 @@
+package com.example.java;
+
+public class StackByNode {
+  private ListNode head;
+
+  public void push(int data) {
+    if (head == null) {
+      head = new ListNode(data);
+    }
+    head.next = new ListNode(data);
+  }
+
+  public int pop() {
+    if (head == null) {
+      return -1;
+    }
+    ListNode node = head;
+    if (head.next == null) {
+      head = null;
+      return node.data;
+    }
+
+    while (node.next.next != null) {
+      node = node.next;
+    }
+
+    int pushValue = node.next.data;
+    node.next = null;
+
+    return pushValue;
+  }
+}
