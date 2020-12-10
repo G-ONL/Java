9ac5f175911d03e1159c4dd9645fd15429964a0c
kimjaeyun <je941004@naver.com>
2020-12-10 22:55:44 +0900

    GitHub Issue Calculator 구현

---
+++ src/main/java/com/example/java/IssueCalculator.java
@@ -0,0 +1,58 @@
+package com.example.java;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+public class IssueCalculator {
+
+  private String token = "토큰값";
+  private static final String WHITESHIP = "whiteship/live-study";
+  private static final int CONTENTS_NUMBER = 18;
+
+  public void start() throws IOException {
+    HashMap<String, Integer> peopleCount = new HashMap<>();
+
+    GitHub certifiedGitHub = authenticate();
+    countPeople(certifiedGitHub, peopleCount);
+    calculatePartitionRate(peopleCount);
+  }
+
+  private GitHub authenticate() throws IOException {
+    System.out.println("----인증");
+    return new GitHubBuilder().withOAuthToken(token).build();
+  }
+
+  private void countPeople(GitHub gitHub, HashMap<String, Integer> peopleCount) throws IOException {
+    for (int issueId = 1; issueId <= CONTENTS_NUMBER; issueId++) {
+      List<GHIssueComment> issues = getCommentsByRepositoryAndIssue(gitHub, WHITESHIP, issueId);
+      for (GHIssueComment issueComment : issues) {
+        String loginUser = issueComment.getUser().getLogin();
+        Integer count = peopleCount.get(loginUser);
+        peopleCount.put(loginUser, count == null ? 1 : ++count);
+      }
+    }
+  }
+
+  private void calculatePartitionRate(HashMap<String, Integer> peopleCount) {
+    System.out.println("---------------------------------------------");
+    peopleCount.forEach((name, count) -> {
+      System.out.printf("%s %.2f%%", name, Math.round((count * 10000)) / (CONTENTS_NUMBER * 100.0) );
+      System.out.println();
+      System.out.println("---------------------------------------------");
+    });
+  }
+
+
+  private List<GHIssueComment> getCommentsByRepositoryAndIssue(GitHub gitHub,
+      String repositoryName, int issueId)
+      throws IOException {
+    System.out.println("----이슈 연결 " + issueId + "번째");
+    return gitHub.getRepository(repositoryName).getIssue(issueId)
+        .getComments();
+  }
+
+}
--- src/main/java/com/example/java/JavaApplication.java
+++ src/main/java/com/example/java/JavaApplication.java
@@ -1,13 +1,17 @@
 package com.example.java;
 
+import java.io.IOException;
+import java.util.HashMap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(JavaApplication.class, args);
+  public static void main(String[] args) throws IOException {
+    IssueCalculator issueCalculator = new IssueCalculator();
+    issueCalculator.start();
+//    SpringApplication.run(JavaApplication.class, args);
   }
 
 }
